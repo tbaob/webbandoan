a026b5f6c4fd6047f6ca69b4659fd6a3d3d97244
ADMIN <ADMIN@LAPTOP-3N9DMSHG>
2024-10-14 22:20:00 +0700

    Add project files.

---
+++ webbandoan/Controllers/ProductController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webbandoan.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Combo1Detail()
+        {
+            return View();
+        }
+        public ActionResult Combo2Detail()
+        {
+            return View();
+        }
+        public ActionResult Combo3Detail()
+        {
+            return View();
+        }
+        public ActionResult Combo4Detail()
+        {
+            return View();
+        }
+        public ActionResult Combo6Detail()
+        {
+            return View();
+        }
+        public ActionResult Combo7Detail()
+        {
+            return View();
+        }
+        public ActionResult Combo9Detail()
+        {
+            return View();
+        }
+        public ActionResult Combo110Detail()
+        {
+            return View();
+        }
+        public ActionResult Combo11Detail()
+        {
+            return View();
+        }
+        public ActionResult Combo12Detail()
+        {
+            return View();
+        }
+        public ActionResult Combo13Detail()
+        {
+            return View();
+        }
+        public ActionResult Combo14Detail()
+        {
+            return View();
+        }
+        public ActionResult Combo15Detail()
+        {
+            return View();
+        }
+        public ActionResult Combo16Detail()
+        {
+            return View();
+        }
+        public ActionResult Combo17Detail()
+        {
+            return View();
+        }
+        public ActionResult Combo18Detail()
+        {
+            return View();
+        }
+        public ActionResult Combo19Detail()
+        {
+            return View();
+        }
+        public ActionResult Combo20Detail()
+        {
+            return View();
+        }
+    }
+}
